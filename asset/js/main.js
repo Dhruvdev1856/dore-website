db6ce0e75a9e573502414e0f95abf83a10eb4af9
Dhruv <dhruvdev1856@gmail.com>
2023-10-09 21:48:43 +0530

    made main page design

---
+++ asset/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.click').click(function(){
+        $('.theme-changer').toggleClass('theme-changer1');
+    });
+    $('.click').click(function(){
+        $('.click').toggleClass('none');
+    });
+});
